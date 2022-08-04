12082da246258f3c395506ba8dfd26c18b5c5503
SAMI <kk465kk@gmail.com>
2022-08-03 18:19:35 -0700

    z

---
+++ Resit-SAMI-NIAZI/src/main/java/com/example/Resit/SAMI/NIAZI/ResitSamiNiaziApplication.java
@@ -0,0 +1,13 @@
+package com.example.Resit.SAMI.NIAZI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResitSamiNiaziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResitSamiNiaziApplication.class, args);
+	}
+
+}
---
+++ Resit-SAMI-NIAZI/src/test/java/com/example/Resit/SAMI/NIAZI/ResitSamiNiaziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Resit.SAMI.NIAZI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResitSamiNiaziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
